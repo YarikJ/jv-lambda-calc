2ec76eccd7d00386b43e127340c16e579ad77f80
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2020-02-19 10:05:22 +0200

    rename constant delta, add assert on 10 to the power of  0

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -8,45 +8,46 @@ import org.junit.rules.ExpectedException;
 public class CalculatorTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
-    private static final double delta = 0.0;
+    private static final double DELTA = 0.0;
 
 
     @Test
     public void sumOperationTest() {
-        Assert.assertEquals(7, Calculator.calculate(5, 2, '+'), delta);
-        Assert.assertEquals(7, Calculator.calculate(2, 5, '+'), delta);
-        Assert.assertEquals(-7, Calculator.calculate(-2, -5, '+'), delta);
+        Assert.assertEquals(7, Calculator.calculate(5, 2, '+'), DELTA);
+        Assert.assertEquals(7, Calculator.calculate(2, 5, '+'), DELTA);
+        Assert.assertEquals(-7, Calculator.calculate(-2, -5, '+'), DELTA);
     }
 
     @Test
     public void subtractionOperationTest() {
-        Assert.assertEquals(3, Calculator.calculate(5, 2, '-'), delta);
-        Assert.assertEquals(-3, Calculator.calculate(2, 5, '-'), delta);
-        Assert.assertEquals(3, Calculator.calculate(-2, -5, '-'), delta);
+        Assert.assertEquals(3, Calculator.calculate(5, 2, '-'), DELTA);
+        Assert.assertEquals(-3, Calculator.calculate(2, 5, '-'), DELTA);
+        Assert.assertEquals(3, Calculator.calculate(-2, -5, '-'), DELTA);
     }
 
     @Test
     public void multiplyOperationTest() {
-        Assert.assertEquals(10, Calculator.calculate(5, 2, '*'), delta);
-        Assert.assertEquals(10, Calculator.calculate(2, 5, '*'), delta);
-        Assert.assertEquals(10, Calculator.calculate(-2, -5, '*'), delta);
-        Assert.assertEquals(-10, Calculator.calculate(-2, 5, '*'), delta);
+        Assert.assertEquals(10, Calculator.calculate(5, 2, '*'), DELTA);
+        Assert.assertEquals(10, Calculator.calculate(2, 5, '*'), DELTA);
+        Assert.assertEquals(10, Calculator.calculate(-2, -5, '*'), DELTA);
+        Assert.assertEquals(-10, Calculator.calculate(-2, 5, '*'), DELTA);
     }
 
     @Test
     public void divisionOperationTest() {
-        Assert.assertEquals(5, Calculator.calculate(10, 2, '/'), delta);
-        Assert.assertEquals(2, Calculator.calculate(10, 5, '/'), delta);
-        Assert.assertEquals(2, Calculator.calculate(-10, -5, '/'), delta);
-        Assert.assertEquals(-2, Calculator.calculate(-10, 5, '/'), delta);
+        Assert.assertEquals(5, Calculator.calculate(10, 2, '/'), DELTA);
+        Assert.assertEquals(2, Calculator.calculate(10, 5, '/'), DELTA);
+        Assert.assertEquals(2, Calculator.calculate(-10, -5, '/'), DELTA);
+        Assert.assertEquals(-2, Calculator.calculate(-10, 5, '/'), DELTA);
     }
 
     @Test
     public void powOperationTest() {
-        Assert.assertEquals(100, Calculator.calculate(10, 2, '^'), delta);
-        Assert.assertEquals(32, Calculator.calculate(2, 5, '^'), delta);
-        Assert.assertEquals(-0.125, Calculator.calculate(-2, -3, '^'), delta);
-        Assert.assertEquals(-100000, Calculator.calculate(-10, 5, '^'), delta);
+        Assert.assertEquals(100, Calculator.calculate(10, 2, '^'), DELTA);
+        Assert.assertEquals(32, Calculator.calculate(2, 5, '^'), DELTA);
+        Assert.assertEquals(-0.125, Calculator.calculate(-2, -3, '^'), DELTA);
+        Assert.assertEquals(-100000, Calculator.calculate(-10, 5, '^'), DELTA);
+        Assert.assertEquals(1, Calculator.calculate(-10, 0, '^'), DELTA);
     }
 
     @Test
